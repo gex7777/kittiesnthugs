02ba6a9b475c8822c6eeb62b5c454c3297c91228
george menezes <gex7777@gmail.com>
2020-07-23 22:54:28 +0530

    Updates

---
+++ precache-manifest.1af1dbd50ed93ec9c7d25c801567559e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fc86a6f99175274a81d7e69de8d1b34",
+    "url": "/kittiesnthugs/index.html"
+  },
+  {
+    "revision": "1b809991ffe06921765e",
+    "url": "/kittiesnthugs/static/css/main.fb82b465.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/kittiesnthugs/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kittiesnthugs/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b809991ffe06921765e",
+    "url": "/kittiesnthugs/static/js/main.df080169.chunk.js"
+  },
+  {
+    "revision": "997e314c7c268280dfdb",
+    "url": "/kittiesnthugs/static/js/runtime-main.4c07fc59.js"
+  }
+]);
